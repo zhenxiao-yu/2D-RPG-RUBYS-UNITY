d34bb89d4be4333b758364f15b90764f38ffd5f0
Mark Yu <zyu347@uwo.ca>
2023-09-29 14:12:11 -0400

    add vertical movement

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
-    }
+   // Start is called before the first frame update
+   void Start()
+   { 
+   }
+   // Update is called once per frame
+   void Update()
+   {
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+       
+       Vector2 position = transform.position;
+       position.x = position.x + 0.1f * horizontal;
+       position.y = position.y + 0.1f * vertical;
+       transform.position = position;
+   }
 }
