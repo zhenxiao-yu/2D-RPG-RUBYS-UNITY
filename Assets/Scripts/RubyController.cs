c14d84dace37cbf74af22767de727046b299b636
Mark Yu <zyu347@uwo.ca>
2023-09-29 19:51:27 -0400

    updated map

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,16 +6,20 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-
+    
     public int maxHealth = 5;
-
+    public float timeInvincible = 2.0f;
+    
+    public int health { get { return currentHealth; }}
     int currentHealth;
-    public int health { get { return currentHealth; } }
-
+    
+    bool isInvincible;
+    float invincibleTimer;
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +32,15 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
-
+    
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
@@ -41,7 +52,17 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
+
