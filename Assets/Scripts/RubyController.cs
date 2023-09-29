c47e9b355370f96aa2f21873886e1fe0eb39edb4
Mark Yu <zyu347@uwo.ca>
2023-09-29 14:19:18 -0400

    Update RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,7 +6,7 @@ public class RubyController : MonoBehaviour
 {
    // Start is called before the first frame update
    void Start()
-   { 
+   {
    }
    // Update is called once per frame
    void Update()
@@ -15,8 +15,8 @@ public class RubyController : MonoBehaviour
        float vertical = Input.GetAxis("Vertical");
        
        Vector2 position = transform.position;
-       position.x = position.x + 0.1f * horizontal;
-       position.y = position.y + 0.1f * vertical;
+       position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+       position.y = position.y + 3.0f * vertical * Time.deltaTime;
        transform.position = position;
    }
 }
